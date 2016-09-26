21088aa3ebe902d8d19ed248d100b2b7daf3dcfb
jjdebuchy <juanjodebuchy@gmail.com>
2016-09-26 19:33:28 -0300

    validacion.js - noe

--- validacion.js
+++ validacion.js
@@ -11,25 +11,76 @@ window.onload = function(){
     e.preventDefault();
 
     function validar(){
+
       //validacion del nombre
+
       if(trim(inputNombre.value).length == 0 || typeOf(inputNombre) !== string ){
+
         alert("Por favor completa el nombre";
+
         errores.push("Nombre incompleto");
+
       }
+
       if(trim(inputApellido.value).length == 0 || typeOf(inputApellido) !== string ){
+
         alert("Por favor completa el Apellido");
+
         errores.push("Apellido sin completar");
+
       }
+
+      if(trim(inputMail.value).length == 0 || typeOf(inputMail) !== string ){
+
+        alert("Por favor completa el Mail");
+
+        errores.push("Mail sin completar");
+
+      }
+
+      var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+      if (!re.test(trim(inputMail.value))) {
+        errores.push("Mail no valido");
+      }
+
+      if(trim(inputTelefono.value).length == 0 || typeOf(inputTelefono) !== string ){
+
+        alert("Por favor completa el Telefono");
+        errores.push("Telefono sin completar");
+
+      }
+
+      var telefono = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        if(!inputTelefono.value.match(telefono))
+           {
+             errores.push("Telefono no valido");
+           }
+
       if(trim(inputClave.value).length < 7 ){
-        alert("Por favor completa el Apellido");
+
+        alert("Por favor completa la clave");
+
         errores.push("Clave sin completar");
+
       }
+
+      if(trim(inputClave.value)!== trim(inputRClave.value)){
+
+        alert("Claves NO coincidentes");
+
+        errores.push("Claves no coincidentes");
+
+      }
+
       if(errores.length == 0){
+
         registrar();
         alert("Gracias por registrarte, sos el usuario " + mostrarUsuarios());
-      }
-    }
 
+      }
+
+  }
     function registrar(){
       var sumarNuevoUsuario = new XMLHttpRequest();
 
